369ac28cb9a70bc4f62d27afc24d45cc295fdaae
infwqy759 <wqy759@haw-hamburg.de>
2023-10-31 19:15:02 +0100

    Initial commit

---
+++ src/main/java/elias/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package elias.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
